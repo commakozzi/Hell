40c729da09e88830a84539858df454cc2722fe4d
commakozzi <commakozzi1@gmail.com>
2016-05-31 23:31:23 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        // Start Utilities:
+        Scanner scanner = new Scanner(System.in);
+
+        // Get userName and start game:
+
+    }
+
+    public void typeText(final String str) {
+        new Thread() {
+            public void run() {
+                for(int i = 0; i < str.length(); i++) {
+                }
+            }
+        };
+    }
+}
